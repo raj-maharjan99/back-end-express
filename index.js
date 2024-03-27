b2e5df96083321ddcfe2d77dd4ef4bce1034746b
=raj-maharjan99 <=rjuallone@gmail.com>
2024-03-27 02:06:53 -0500

    this is the way to get image document types

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-const data = require("./data");
-
-console.log(data.data);
-console.log(data.user)
-
-// this is the way to import multiple data  from a single file in nodejs
+const path = require('path')
+const image =  'raj.jpg'
+const document = 'resume.pdf';
+console.log(path.extname(document, image)) // output  .jpg it means this method use to get image or any kind of data types
