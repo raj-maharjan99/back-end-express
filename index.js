83d24ac497e180488c837857d7f66aced9b1404f
=raj-maharjan99 <=rjuallone@gmail.com>
2024-03-27 03:08:41 -0500

    create different api

--- index.js
+++ index.js
@@ -6,13 +6,34 @@ const port = 5000;
 console.log(product)
 
 
-app.get('/', (req, res)=> {
-console.log('api calling')
-return res.status(200).send('Hello World!')
+app.get('/products', (req, res)=> {
+console.log('get api is calling')
+return res.status(200).json({
+    status: 'success',
+    data: product
 })
+})
+app.post('/products', (req, res)=> {
+    console.log('post api is  calling')
+    return res.status(200).json({
+        status: 'post success',
+        data: product
+    })
+    })
+
+    //Most Imp ****************
+    // api by id with dynamic way
+    app.get('/products/:id', (req, res)=> {
+        const {id}= req.params;
+      const item = product.find((x)=> x.id === parseInt(id))
+        return res.status(200).json({
+            status: 'successfully  got the data by id',
+            data: item
+        })
+        })
 
 app.listen(port,()=> {
-    console.log('server is running on localhost 5000')
+   
 })
 
 // this the way to run in local server 
