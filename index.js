a54d009b31b6545de6f354843dc2c70ab4a65081
=raj-maharjan99 <=rjuallone@gmail.com>
2024-03-27 02:35:59 -0500

    install express npm i express and run it on local server  5000

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
-const path = require('path')
-const image =  'raj.jpg'
-const document = 'resume.pdf';
-console.log(path.extname(document, image)) // output  .jpg it means this method use to get image or any kind of data types
+const express = require('express');
+const app = express()
+const port = 5000;
+
+const products = [
+    { id: '1', name:""}
+]
+
+
+app.get('/', (req, res)=> {
+console.log('api calling')
+return res.status(200).send('Hello World!')
+})
+
+app.listen(port,()=> {
+    console.log('server is running on localhost 5000')
+})
+
+// this the way to run in local server 
